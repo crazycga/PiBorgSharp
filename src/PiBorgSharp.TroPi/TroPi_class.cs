16551c977cddd1128f1335bfb57a3ece3e9a3f65
crazycga <crazycga@gmail.com>
2021-03-31 18:52:22 -0600

    Adding TroPi project spec

---
+++ src/PiBorgSharp.TroPi/TroPi_class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiBorgSharp.TroPi
+{
+    public class TroPi_class
+    {
+        public TroPi_class()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
